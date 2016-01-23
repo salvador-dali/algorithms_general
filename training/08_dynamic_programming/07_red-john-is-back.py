661f010072811f0e0b674a0fac302d432967d3a9
Dmytro Krasnoshtan <dmytro@knowlabs.com>
2016-01-23 01:50:11 -0800

    dp

---
+++ training/08_dynamic_programming/07_red-john-is-back.py
@@ -0,0 +1,30 @@
+# https://www.hackerrank.com/challenges/red-john-is-back
+def numOfPossibilities(n):
+    arr = [1, 1, 1, 2]
+    if n < 5:
+        return arr[n - 1]
+
+    i, num = 0, n - 4
+    while i < num:
+        arr.append(arr[-1] + arr[-4])
+        i += 1
+
+    return arr[-1]
+
+# Sieve of Eratosthenes
+# http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+def sieve(n):
+    import math
+    nums = [1] * (n + 1)
+    nums[:2] = [0] * 2
+    nums[4::2] = [0] * int(math.ceil((n - 3) / 2.0))
+    for i in range(3, int((n + 1)**.5) + 1, 2):
+        if nums[i]:
+            for j in range(i*i, n+1, 2*i):
+                nums[j] = 0
+
+    return [i for i, k in enumerate(nums) if k]
+
+
+for i in xrange(int(raw_input())):
+    print len(sieve(numOfPossibilities(input())))
---
+++ training/08_dynamic_programming/09_play-game.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/play-game
+# suppose the numbers are
+# 6, 8, 1, 1, 6 and their positions
+# 0, 1, 2, 3, 4
+# looking at them from the reverse order, one can see that if we are now in the position 4,
+# the best play is to grab 6, in position 3 then 1 + 6, position 2 - then 1 + 1 + 6. In all the cases the opponent will
+# have 0
+# if we are in position 1, then we can take:
+# only 8 which means that our opponent will take the best for him (all values and gives use 0)
+# we will populate our DP table in reverse order where keys are positions and
+# values - are [competitorBestScore, yourBestScore]
+def playGame(arr):
+    l = len(arr)
+    if l < 4:
+        return sum(arr)
+
+    bestScore = {
+        l - 1: [arr[l - 1], 0],
+        l - 2: [arr[l - 1] + arr[l - 2], 0],
+        l - 3: [arr[l - 1] + arr[l - 2] + arr[l - 3], 0]
+    }
+    for i in xrange(3, l):
+        pos = l - i - 1
+        ways = [[sum(arr[pos:pos + i]) + bestScore[pos + i][1], bestScore[pos + i][0]] for i in xrange(1, 4)]
+        best = sorted(ways, key=lambda x: -x[0])
+        bestScore[pos] = best[0]
+
+    return bestScore[0]
+
+for i in xrange(input()):
+    raw_input()
+    print playGame(map(int, raw_input()))[0]
