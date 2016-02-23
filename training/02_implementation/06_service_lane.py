036f2fcd5de11cc8ca0af099b290221b6dd156ca
Dmytro Krasnoshtan <dmytro@knowlabs.com>
2016-02-22 23:52:03 -0800

    renaming and add few tasks

--- training/02_implementation/06_service_lane.py
+++
@@ -1,7 +0,0 @@
-# https://www.hackerrank.com/challenges/service-lane
-num = map(int, raw_input().split())[1]
-arr = list(map(int, raw_input().split()))
-
-for k in range(num):
-    i, j = map(int, raw_input().split())
-    print min(arr[e] for e in range(i, j+1))
