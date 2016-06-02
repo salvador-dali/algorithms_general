03db38f01c5000f576a1b85f0054ef8a9ce3fecf
Dmytro Krasnoshtan <dmytro@knowlabs.com>
2016-06-02 02:12:46 -0700

    game theory competition

---
+++ __competitions/2016/05_13_game_theory/07_day-2-poker-nim.py
@@ -0,0 +1,8 @@
+for i in xrange(input()):
+    n, k = map(int, raw_input().split())
+    arr = map(int, raw_input().split())
+    res = reduce(lambda x,y: x^y, arr)
+    if res == 0:
+        print 'Second'
+    else:
+        print 'First'
